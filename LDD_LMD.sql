18b7451cb0015b8e5341e9ae0fd3e8e3d47bb4b3
Olivier <oliviernadeau2003@gmail.com>
2025-04-09 23:18:47 -0400

    Debut of database SQL code

--- LDD_LMD.sql
+++ LDD_LMD.sql
@@ -1,25 +1,85 @@
 -- Olivier Nadeau, Yonah Lahlou, Ahmadou Ayande, ...
 -- Définition de la base en LDD et LMD de SQL server/ORACLE
 
-
 CREATE DATABASE Projet_Session_Centre_Aide;
 USE Projet_Session_Centre_Aide;
 
 -- Création des tables :
 
-DROP TABLE IF EXISTS Intervention;
 DROP TABLE IF EXISTS Plainte;
-DROP TABLE IF EXISTS Secteur;
-DROP TABLE IF EXISTS Usager;
+DROP TABLE IF EXISTS Intervention;
 DROP TABLE IF EXISTS Equipe;
+DROP TABLE IF EXISTS Usager;
 DROP TABLE IF EXISTS Benevole;
+DROP TABLE IF EXISTS Secteur;
 
-CREATE TABLE Usager();
-CREATE TABLE Benevole();
-CREATE TABLE Equipe();
-CREATE TABLE Secteur();
-CREATE TABLE Plainte();
-CREATE TABLE Intervention();
+
+CREATE TABLE Secteur(
+    id INT,
+    nom VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_id_secteur PRIMARY KEY (id)
+);
+
+CREATE TABLE Usager(
+    id INT,
+    nom VARCHAR(25) NOT NULL,
+    prenom VARCHAR(25) NOT NULL,
+    telephone VARCHAR(10) NOT NULL CHECK (LEN(telephone) = 10 AND telephone NOT LIKE '%[^0-9]%'),
+    courriel VARCHAR(50) NOT NULL CHECK (email LIKE '_%@_%._%'),
+    adresse VARCHAR(50) NOT NULL,
+    id_secteur INT,
+    CONSTRAINT pk_usager_id PRIMARY KEY (id),
+    CONSTRAINT fk_usager_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+);
+
+CREATE TABLE Benevole(
+    id INT,
+    nom VARCHAR(25) NOT NULL,
+    prenom VARCHAR(25) NOT NULL,
+    telephone VARCHAR(10) NOT NULL CHECK (LEN(telephone) = 10 AND telephone NOT LIKE '%[^0-9]%'),
+    courriel VARCHAR(50) NOT NULL CHECK (email LIKE '_%@_%._%'),
+    adresse VARCHAR(50) NOT NULL,
+    possede_voiture BOOLEAN NOT NULL,
+    id_secteur INT,
+    CONSTRAINT pk_benevole_id PRIMARY KEY (id),
+    CONSTRAINT fk_benevole_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+);
+
+CREATE TABLE Equipe(
+    id INT,
+    est_disponible BOOLEAN NOT NULL,
+    id_benevole_1 INT,
+    id_benevole_2 INT,
+    id_secteur INT,
+    CONSTRAINT pk_equipe_id PRIMARY KEY (id),
+    CONSTRAINT fk_equipe_id_benevole_1 FOREIGN KEY (id_benevole_1) REFERENCES Benevole(id),
+    CONSTRAINT fk_equipe_id_benevole_2 FOREIGN KEY (id_benevole_2) REFERENCES Benevole(id),
+    CONSTRAINT fk_equipe_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+);
+
+CREATE TABLE Intervention(
+    id INT,
+    date_demande DATETIME NOT NULL,
+    date_intervention DATETIME NOT NULL,
+    type_aide VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    id_usager INT,
+    id_equipe INT,
+    id_secteur INT,
+    CONSTRAINT pk_intervention_id PRIMARY KEY (id),
+    CONSTRAINT fk_intervention_id_usager FOREIGN KEY (id_usager) REFERENCES Usager(id),
+    CONSTRAINT fk_intervention_id_equipe FOREIGN KEY (id_equipe) REFERENCES Equipe(id),
+    CONSTRAINT fk_intervention_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+);
+
+CREATE TABLE Plainte(
+    id INT,
+    type VARCHAR(50) NOT NULL,
+    description TEXT,
+    id_intervention INT,
+    CONSTRAINT pk_plainte_id PRIMARY KEY (id),
+    CONSTRAINT fk_plainte_id_intervention FOREIGN KEY (id_intervention) REFERENCES Intervention(id)
+);
 
 
 -- Insertion des données : 

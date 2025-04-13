2f1cc219e70f29ee03cca9f092d920f9840becc5
AhmadouA <115052318+AhmadouA@users.noreply.github.com>
2025-04-13 09:39:18 -0400

    T- SQL.sql

--- T- SQL.sql
+++ T- SQL.sql
@@ -1,11 +1,70 @@
--- Olivier Nadeau, Yonah Lahlou, Ahmadou Ayande, ...
--- Ensemble des triggers/curseurs/procédures en Transact-SQL ou PL/SQL
-
--- Trigger : 
+--  4. Transact-SQL (15%)
 
 
--- Procedure Stockée : 
+        -- Implantation d'au moins un déclencheur, une procédure stockée, une fonction
+
+            -- Declencheur-----------------------------------------
+CREATE TRIGGER TR_VerifierVoitureDansEquipe
+ON Equipe
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT e.id
+        FROM inserted e
+        LEFT JOIN Benevole b1 ON e.id_benevole_1 = b1.id
+        LEFT JOIN Benevole b2 ON e.id_benevole_2 = b2.id
+        WHERE (b1.possede_voiture = 0 OR b1.possede_voiture IS NULL)
+          AND (b2.possede_voiture = 0 OR b2.possede_voiture IS NULL)
+    )
+    BEGIN
+        ROLLBACK TRANSACTION
+        RAISERROR('Chaque équipe doit avoir au moins un bénévole avec voiture', 16, 1)
+        RETURN
+    END
+END;
+GO
 
 
--- Fonction Stockée : 
+
+
+            -- une procédure stockée---------------------------------------------------
+CREATE PROCEDURE InsererUsager
+    @id INT,
+    @nom VARCHAR(25),
+    @prenom VARCHAR(25),
+    @telephone VARCHAR(10),
+    @courriel VARCHAR(50),
+    @adresse VARCHAR(50),
+    @id_secteur INT
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                INSERT INTO Usager (id, nom, prenom, telephone, courriel, adresse, id_secteur)
+                VALUES (@id, @nom, @prenom, @telephone, @courriel, @adresse, @id_secteur);
+            END; 
+GO 
+
+
+
+
+            -- une fonction------------------------------------------------
+CREATE FUNCTION fn_NombreInterventionsParMois 
+(
+    @Annee INT,
+    @Mois INT
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @NombreInterventions INT;
+    
+    SELECT @NombreInterventions = COUNT(*)
+    FROM Intervention
+    WHERE YEAR(DateIntervention) = @Annee
+    AND MONTH(DateIntervention) = @Mois;
+    
+    RETURN @NombreInterventions;
+END;
+GO 
 

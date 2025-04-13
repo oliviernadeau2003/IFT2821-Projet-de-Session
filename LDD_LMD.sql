b4d2bbf9bce817c20c834f94b3c3f0e4ef5a48c0
AhmadouA <115052318+AhmadouA@users.noreply.github.com>
2025-04-13 07:06:37 -0400

    LDD_LMD.sql

--- LDD_LMD.sql
+++ LDD_LMD.sql
@@ -20,6 +20,7 @@ CREATE TABLE Secteur(
     CONSTRAINT pk_id_secteur PRIMARY KEY (id)
 );
 
+
 CREATE TABLE Usager(
     id INT,
     nom VARCHAR(25) NOT NULL,
@@ -32,6 +33,7 @@ CREATE TABLE Usager(
     CONSTRAINT fk_usager_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
 );
 
+
 CREATE TABLE Benevole(
     id INT,
     nom VARCHAR(25) NOT NULL,
@@ -40,45 +42,55 @@ CREATE TABLE Benevole(
     courriel VARCHAR(50) NOT NULL CHECK (courriel LIKE '_%@_%._%'),
     adresse VARCHAR(50) NOT NULL,
     possede_voiture BIT NOT NULL,
-    id_secteur INT NOT NULL,
+    id_secteur INT,
     CONSTRAINT pk_benevole_id PRIMARY KEY (id),
     CONSTRAINT fk_benevole_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
 );
 
-CREATE TABLE Unite(
+
+CREATE TABLE Equipe(
     id INT,
-    disponible BIT NOT NULL,
+    est_disponible BIT NOT NULL,
     id_benevole_1 INT NOT NULL,
-    id_benevole_2 INT,
+    id_benevole_2 INT, 
     id_secteur INT NOT NULL,
-    CONSTRAINT pk_unite_id PRIMARY KEY (id),
-    CONSTRAINT fk_unite_id_benevole_1 FOREIGN KEY (id_benevole_1) REFERENCES Benevole(id),
-    CONSTRAINT fk_unite_id_benevole_2 FOREIGN KEY (id_benevole_2) REFERENCES Benevole(id),
-    CONSTRAINT fk_unite_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+    type_equipe VARCHAR(20) NOT NULL CHECK (type_equipe IN ('preformee', 'a_constituer')),
+    CONSTRAINT pk_equipe_id PRIMARY KEY (id),
+    CONSTRAINT fk_equipe_id_benevole_1 FOREIGN KEY (id_benevole_1) REFERENCES Benevole(id),
+    CONSTRAINT fk_equipe_id_benevole_2 FOREIGN KEY (id_benevole_2) REFERENCES Benevole(id),
+    CONSTRAINT fk_equipe_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
 );
+
 
 CREATE TABLE Intervention(
     id INT,
     date_demande DATETIME NOT NULL,
     date_intervention DATETIME NOT NULL,
     type_aide VARCHAR(50) NOT NULL,
-    statut VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    duree INT NULL,
     id_usager INT,
-    id_unite INT,
+    id_equipe INT,
     id_secteur INT,
     CONSTRAINT pk_intervention_id PRIMARY KEY (id),
     CONSTRAINT fk_intervention_id_usager FOREIGN KEY (id_usager) REFERENCES Usager(id),
-    CONSTRAINT fk_intervention_id_unite FOREIGN KEY (id_unite) REFERENCES Unite(id),
-    CONSTRAINT fk_intervention_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id)
+    CONSTRAINT fk_intervention_id_equipe FOREIGN KEY (id_equipe) REFERENCES Equipe(id),
+    CONSTRAINT fk_intervention_id_secteur FOREIGN KEY (id_secteur) REFERENCES Secteur(id),
+    CONSTRAINT CHK_Status CHECK (status IN ('Pas encore commencé', 'En cours', 'Terminé', 'Annulé'))
 );
 
 CREATE TABLE Plainte(
     id INT,
     type VARCHAR(50) NOT NULL,
-    description TEXT,
+    description VARCHAR(MAX),
+    date_signalement DATETIME DEFAULT GETDATE() NOT NULL,
+    statut VARCHAR(20) DEFAULT 'Ouvert' NOT NULL,
+    date_resolution DATETIME NULL,
+    resolution VARCHAR(MAX) NULL,
     id_intervention INT,
     CONSTRAINT pk_plainte_id PRIMARY KEY (id),
-    CONSTRAINT fk_plainte_id_intervention FOREIGN KEY (id_intervention) REFERENCES Intervention(id)
+    CONSTRAINT fk_plainte_id_intervention FOREIGN KEY (id_intervention) REFERENCES Intervention(id),
+    CONSTRAINT CHK_Statut_Plainte CHECK (statut IN ('Ouvert', 'En traitement', 'Résolu', 'Fermé'))
 );
 
 

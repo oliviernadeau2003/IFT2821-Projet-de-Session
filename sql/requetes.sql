72e9c625e57250a0598045fc44c1ab4b995043f4
Olivier Nadeau <oliviernadeau2003@gmail.com>
2025-04-14 17:44:23 -0400

    change equipe par unite

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -13,7 +13,7 @@ WHERE possede_voiture = 1;
 --R1.3 : Afficher toutes les interventions dont le statut est "Termine".
 SELECT *
 FROM Intervention
-WHERE status = 'Termine';
+WHERE statut = 'Termine';
 
 --R1.4 : Afficher toutes les plaintes dont le statut est "Ouvert".
 SELECT *
@@ -30,13 +30,13 @@ ORDER BY nom;
 SELECT Intervention.id AS id_intervention,
        Usager.nom AS nom_usager,
        SecteurUsager.nom AS secteur_usager,
-       SecteurEquipe.nom AS secteur_equipe
+       SecteurUnite.nom AS secteur_unite
 FROM Intervention
 JOIN Usager ON Intervention.id_usager = Usager.id
-JOIN Equipe ON Intervention.id_equipe = Equipe.id
+JOIN Unite ON Intervention.id_Unite = Unite.id
 JOIN Secteur AS SecteurUsager ON Usager.id_secteur = SecteurUsager.id
-JOIN Secteur AS SecteurEquipe ON Equipe.id_secteur = SecteurEquipe.id
-WHERE SecteurUsager.id <> SecteurEquipe.id;
+JOIN Secteur AS SecteurUnite ON Unite.id_secteur = SecteurUnite.id
+WHERE SecteurUsager.id <> SecteurUnite.id;
 
 --R2.2 Pour chaque intervention :
     -- Afficher l'identifiant de l'intervention, la date d'intervention, le nom de l'usager, le secteur auquel l'usager appartient et les noms des benevoles intervenants (le benevole principal et, le cas echeant, le second).
@@ -49,23 +49,23 @@ SELECT Intervention.id AS id_intervention,
 FROM Intervention
 JOIN Usager ON Intervention.id_usager = Usager.id
 JOIN Secteur ON Usager.id_secteur = Secteur.id
-JOIN Equipe ON Intervention.id_equipe = Equipe.id
-JOIN Benevole AS BenevolePrincipal ON Equipe.id_benevole_1 = BenevolePrincipal.id
-LEFT JOIN Benevole AS BenevoleSecondaire ON Equipe.id_benevole_2 = BenevoleSecondaire.id;
+JOIN Unite ON Intervention.id_Unite = Unite.id
+JOIN Benevole AS BenevolePrincipal ON Unite.id_benevole_1 = BenevolePrincipal.id
+LEFT JOIN Benevole AS BenevoleSecondaire ON Unite.id_benevole_2 = BenevoleSecondaire.id;
 
---R2.3 Pour chaque intervention effectuee par une equipe de type "preformee" :
-    -- Afficher l'identifiant et la date de l'intervention, le nom de l'usager, le secteur d'affectation de l'equipe et le statut de disponibilite.
+--R2.3 Pour chaque intervention effectuee par une Unite de type "preformee" :
+    -- Afficher l'identifiant et la date de l'intervention, le nom de l'usager, le secteur d'affectation de l'Unite et le statut de disponibilite.
 SELECT Intervention.id AS id_intervention,
        Intervention.date_intervention,
        Usager.nom AS nom_usager,
-       SecteurEquipe.nom AS secteur_equipe,
-       Equipe.type_equipe,
-       Equipe.est_disponible
+       SecteurUnite.nom AS secteur_unite,
+       Unite.type_unite,
+       Unite.est_disponible
 FROM Intervention
 JOIN Usager ON Intervention.id_usager = Usager.id
-JOIN Equipe ON Intervention.id_equipe = Equipe.id
-JOIN Secteur AS SecteurEquipe ON Equipe.id_secteur = SecteurEquipe.id
-WHERE Equipe.type_equipe = 'preformee';
+JOIN Unite ON Intervention.id_unite = unite.id
+JOIN Secteur AS SecteurUnite ON Unite.id_secteur = SecteurUnite.id
+WHERE Unite.type_unite = 'preformee';
 
 --R2.4 Pour chaque plainte en statut "Ouvert" :
     -- Afficher l'identifiant et le type de la plainte, la date de signalement, l'identifiant de l'intervention associee, le nom de l'usager ayant demande l'intervention et le secteur auquel cet usager appartient.
@@ -81,7 +81,7 @@ JOIN Usager ON Intervention.id_usager = Usager.id
 JOIN Secteur AS SecteurUsager ON Usager.id_secteur = SecteurUsager.id
 WHERE Plainte.statut = 'Ouvert';
 
---R2.5 : Ici, on relie Benevole au Secteur puis on joint la table Equipe en verifiant si le benevole intervient comme premier ou second membre. Apres, on joint Intervention pour compter l’ensemble des interventions dans lesquelles le benevole a participe.
+--R2.5 : Ici, on relie Benevole au Secteur puis on joint la table Unite en verifiant si le benevole intervient comme premier ou second membre. Apres, on joint Intervention pour compter l’ensemble des interventions dans lesquelles le benevole a participe.
 SELECT 
     Benevole.id AS id_benevole,
     Benevole.nom AS nom_benevole,
@@ -89,6 +89,6 @@ SELECT
     COUNT(Intervention.id) AS nb_interventions
 FROM Benevole
 JOIN Secteur AS SecteurBenevole ON Benevole.id_secteur = SecteurBenevole.id
-JOIN Equipe ON (Benevole.id = Equipe.id_benevole_1 OR Benevole.id = Equipe.id_benevole_2)
-JOIN Intervention ON Equipe.id = Intervention.id_equipe
+JOIN Unite ON (Benevole.id = Unite.id_benevole_1 OR Benevole.id = Unite.id_benevole_2)
+JOIN Intervention ON Unite.id = Intervention.id_unite
 GROUP BY Benevole.id, Benevole.nom, SecteurBenevole.nom;
